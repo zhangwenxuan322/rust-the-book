5b51ca66a190db817dccfc39dcd8a860c4cc2946
Oscar <ozhang@evolvefm.com.au>
2025-12-19 12:50:26 +1100

    Finish variables part.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,7 @@ fn main() {
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+
+    const SECONDS_IN_HOUR: u32 = 60 * 60;
+    println!("Seconds in an hour: {SECONDS_IN_HOUR}");
 }
